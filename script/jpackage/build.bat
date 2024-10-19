a7e4de768ba5399c95eb15ef2ed2e482d783abc3
Yeding <kanaumachi@gmail.com>
2024-10-19 23:20:56 +0900

    roll back the version for original

--- script/jpackage/build.bat
+++ script/jpackage/build.bat
@@ -6,9 +6,7 @@ rd /S /Q ".\LabelPlusFX"
 set MODULES="%DIR%\target\build"
 set ICON="%DIR%\images\icons\cat.ico"
 
-jpackage --verbose --type app-image --app-version 2.3.4 --copyright "Meodinger Tech (C) 2022" --name LabelPlusFX --icon %ICON% --dest . --module-path %MODULES% --add-modules lpfx,jdk.crypto.cryptoki --module lpfx/ink.meodinger.lpfx.LauncherKt  --java-options "-Dprism.maxvram=2G"
+jpackage --verbose --type app-image --app-version 2.3.3 --copyright "Meodinger Tech (C) 2022" --name LabelPlusFX --icon %ICON% --dest . --module-path %MODULES% --add-modules lpfx,jdk.crypto.cryptoki --module lpfx/ink.meodinger.lpfx.LauncherKt  --java-options "-Dprism.maxvram=2G"
 
 echo:
 echo All completed, remember to copy dlls!
-
-pause
